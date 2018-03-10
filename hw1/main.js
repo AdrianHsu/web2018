ff712ccae6631b1bb697e07a6f50b4e0623218fc
AdrianHsu <adrianhsu1995@gmail.com>
2018-03-11 02:04:58 +0800

    :art: add dlrb...

--- hw1/main.js
+++ hw1/main.js
@@ -129,3 +129,15 @@ function onPlayerStateChange(event) {
 function stopVideo() {
     player.stopVideo();
 }
+
+function dlrb(){
+    openNav();
+}
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
