6d16b127976748fa9cfc4ce095fd473eadad23ea
AdrianHsu <adrianhsu1995@gmail.com>
2018-03-11 01:34:52 +0800

    :art: done, without build playlist

--- hw1/main.js
+++ hw1/main.js
@@ -20,19 +20,37 @@ for(i = 0; i < close.length; i ++) {
 }
 
 var list = document.querySelector('ul');
-console.log(list)
-if(list === null) {
-    console.log("list is null...");
-}
 list.addEventListener('click', function(ev) {
     if(ev.target.tagName == 'LI') {
         ev.target.classList.toggle('checked'); // 點下去之後變成 "checked"
     }
+    console.log(ev.target.tagName);
 }, false);
 
+function refreshLI() {
+    lis = document.getElementsByTagName("li");
+    var i = 0;
+    for(i = 0; i < lis.length; i++) {
+        lis[i].classList.remove('checked');
+    }
+    lis[lis.length - 1].classList.add('checked');
+}
+
+// https://developers.google.com/youtube/iframe_api_reference#Playback_controls
 function newElement() {
+
+    var myurl = document.getElementById("myInput").value;
+    var myid = urlToId(myurl);
+    currentId = myid;
+    player.stopVideo();
+    player.loadVideoById(myid);
+    setTimeout(addElement, 1000);
+}
+function addElement() {
+    console.log("ya!");
+    alert("現在播放：" + currentTitle);
     var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
+    var inputValue = currentId + "|" + currentTitle;
     var t = document.createTextNode(inputValue);
     li.appendChild(t);
     if (inputValue === '') {
@@ -55,21 +73,22 @@ function newElement() {
             div.style.display = "none";
         }
     }
+    refreshLI();
 }
-
-
 var tag = document.createElement('script');
 
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+var currentId = 'HqmpIQ9l-uA';
+var currentTitle = '草東沒有派對 No Party For Cao Dong - 大風吹 Simon Says【Official Music Video】';
 var player;
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
         height: '720',
         width: '1080',
-        videoId: 'HqmpIQ9l-uA',
+        videoId: currentId,
         events: {
         'onReady': onPlayerReady,
         'onStateChange': onPlayerStateChange
@@ -77,17 +96,35 @@ function onYouTubeIframeAPIReady() {
     });
 }
 
+function urlToId(str) {
+    return str.split("v=")[1];
+}
+
 function onPlayerReady(event) {
     event.target.playVideo();
     console.log(event.target.getVideoData().title);
+    currentTitle = event.target.getVideoData().title;
 }
 
-var done = false;
+// var done = false;
 function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-        setTimeout(stopVideo, 6000);
-        done = true;
+    if(event.data === YT.PlayerState.ENDED) {
+        player.loadVideoById(currentId);
+    } else if (event.data === YT.PlayerState.PLAYING) {
+        
+        currentTitle = event.target.getVideoData().title;
+    } else if (event.data === YT.PlayerState.PAUSED) {
+
+    } else if (event.data === YT.PlayerState.BUFFERING) {
+
+    } else if (event.data === YT.PlayerState.CUED) {
+
     }
+    // if (event.data == YT.PlayerState.PLAYING && !done) {
+        // setTimeout(stopVideo, 6000);
+        
+    //     done = true;
+    // }
 }
 function stopVideo() {
     player.stopVideo();
