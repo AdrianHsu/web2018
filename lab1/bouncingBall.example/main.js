7f1c3ea5376a7e21dd34ac4ce647282bf1414722
AdrianHsu <adrianhsu1995@gmail.com>
2018-03-15 01:13:25 +0800

    :zap: bouncing ball done

--- lab1/bouncingBall.example/main.js
+++ lab1/bouncingBall.example/main.js
@@ -1,89 +1,159 @@
-// setup canvas
-
 var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext('2d');
 
 var width = canvas.width = window.innerWidth;
 var height = canvas.height = window.innerHeight;
 
-// function to generate random number
-
-// ctx.fillStyle = "rgba(0,0,0,0.25)";
-// ctx.fillRect (0,0,width, height);
-
-
 function random(min,max) {
   var num = Math.floor(Math.random()*(max-min)) + min;
   return num;
 }
 
-function Ball(x, y, vx, vy, color, r) {
-  this.x = x;
-  this.y = y;
-  this.vx = vx;
-  this.vy = vy;
-  this.color = color;
-  this.r = r;
-}
-
-// 增加一個 ball 的 draw method
-// 給起始點，什麼顏色
-// 圓心 (x, y) 半徑 r, 從0~2pi 所以是圓
-Ball.prototype.draw = function() {
-  ctx.beginPath();
-  ctx.fillStyle = this.color;
-  ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
-  ctx.fill();
-}
-
-Ball.prototype.move = function() {    
-  if(this.x + this.vx < 0 || this.x + this.vx > width) {
-    this.vx = -this.vx;
+class Ball {
+  constructor(x, y, vx, vy, color, r) {
+    this.x = x;
+    this.y = y;
+    this.vx = vx;
+    this.vy = vy;
+    this.color = color;
+    this.r = r; 
   }
-  if(this.y + this.vy < 0 || this.y + this.vy > height) {
-    this.vy = -this.vy;
-  }  
-  this.x += this.vx;
-  this.y += this.vy;
-}
-Ball.prototype.checkCollision = function(m) {
-  for(let i = 0; i < m; i++) {
-    // if(i === m) continue;
-    me = balls[i];
-    var dx_pow = Math.pow(this.x - me.x, 2);
-    var dy_pow = Math.pow(this.y - me.y, 2);
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+  static getDistance(me1, me2) {
+    var dx_pow = Math.pow(me1.x - me2.x, 2);
+    var dy_pow = Math.pow(me1.y - me2.y, 2);
     var dis = Math.sqrt(dx_pow + dy_pow);
-    console.log("dis:" + dis)
-    console.log("r:" + this.r + me.r);
-    if(dis < this.r + me.r){
+    return dis;
+  }
+  move(){    
+    if(this.x + this.vx < 0 || this.x + this.vx > width) {
       this.vx = -this.vx;
+    }
+    if(this.y + this.vy < 0 || this.y + this.vy > height) {
       this.vy = -this.vy;
-      me.vx = -me.vx;
-      me.vy = -me.vy;
-      this.x += BUFFER;
-      this.y += BUFFER; // 讓兩顆撞到的時候馬上分離一小段，
-      // 才不會一直卡在一起
+    }  
+    this.x += this.vx;
+    this.y += this.vy;
+  }
+  set setVx(vx) {
+    this.vx = vx;
+  }
+  set setVy(vy) {
+    this.vy = vy;
+  }
+}
+
+class RegBall extends Ball {
+  checkCollision(m) {
+    for(let i = 0; i < m; i++) {
+      var me = balls[i];
+      var dis = Ball.getDistance(this, me);
+      if(dis < this.r + me.r){
+        this.setVx = -this.vx;
+        this.setVy = -this.vy;
+        me.setVx = -me.vx;
+        me.setVy = -me.vy;
+        this.x += BUFFER;
+        this.y += BUFFER; // to make sure that two balls separate right away
+        // after they collided
+      }
     }
   }
 }
+var audio = new Audio('eatBug.mp3');
+var score = document.getElementById('score');
+var event = new Event('eat');
+
+score.addEventListener('eat', function(e) {
+  total += 1;
+  audio.play();
+  this.innerText = "Score: " + total;
+});
+
+class EvilBall extends Ball {
+  checkCollision(i) {
+    var me = balls[i];
+    var dis = Ball.getDistance(this, me);    
+    if(dis < this.r + me.r){
+      
+      var ball = new RegBall(random(0, width), random(0, height), 
+      random(-VELOCITY, VELOCITY), random(-VELOCITY, VELOCITY), "rgb(" + random(0, 255) + "," + random(0, 255) + "," + random(0, 255) + ")", random(5, 30));
+      balls[i] = ball;
+
+      score.dispatchEvent(event);
+    }  
+  }
+}
+
 var BUFFER = 10;
+var VELOCITY = 10;
 var balls = []
 var TOTAL_NUM = 30;
-var i = 0;
-for(i = 0; i < TOTAL_NUM; i++) {
-  var ball = new Ball(random(0, width), random(0, height), random(-10, 10), random(-10, 10), "rgb(" + random(0, 255) + "," + random(0, 255) + "," + random(0, 255) + ")", random(5, 30));
-  balls[i] = ball;
 
+var ADR_VEL = 10;
+var total = 0;
+
+var ADRIAN_BALL = new EvilBall(random(0, width), 
+random(0, height), 0, 0, "rgb(255,255,255)", 50);
+
+for(let i = 0; i < TOTAL_NUM; i++) {
+  var ball = new RegBall(random(0, width), random(0, height), random(-VELOCITY, VELOCITY), random(-VELOCITY, VELOCITY), "rgb(" + random(0, 255) + "," + random(0, 255) + "," + random(0, 255) + ")", random(5, 30));
+  balls[i] = ball;
 }
 
 function loop() {
-  ctx.fillStyle = 'rgba(0,0,0,0.25)';
+  ctx.fillStyle = 'rgba(15,15,15,0.25)';
   ctx.fillRect(0,0,width,height);
   for(let i = 0; i < TOTAL_NUM; i++) {
     balls[i].draw();
     balls[i].move();
     balls[i].checkCollision(i);
+    ADRIAN_BALL.checkCollision(i);
   }
+  ADRIAN_BALL.draw();
+  ADRIAN_BALL.move();
   requestAnimationFrame(loop);
 }
 loop();
+
+window.addEventListener("keydown", function (event) {
+  if (event.defaultPrevented) {
+    return; // Do nothing if the event was already processed
+  }
+  var vy = 0;
+  var vx = 0;
+  switch (event.key) {
+    case "ArrowDown":
+      // code for "down arrow" key press.
+      vy = +ADR_VEL;
+      vx = 0;
+      break;
+    case "ArrowUp":
+      // code for "up arrow" key press.
+      vy = -ADR_VEL;
+      vx = 0;
+      break;
+    case "ArrowLeft":
+      // code for "left arrow" key press.
+      vx = -ADR_VEL;
+      vy = 0;
+      break;
+    case "ArrowRight":
+      // code for "right arrow" key press.
+      vx = +ADR_VEL;
+      vy = 0;
+      break;
+    default:
+      return; // Quit when this doesn't handle the key event.
+  }
+  ADRIAN_BALL.setVx = vx;
+  ADRIAN_BALL.setVy = vy;
+
+  // Cancel the default action to avoid it being handled twice
+  event.preventDefault();
+}, true);
