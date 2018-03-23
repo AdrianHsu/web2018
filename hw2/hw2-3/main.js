a4abff7221aeeac0525e2c880dc35a5170190195
AdrianHsu <adrianhsu1995@gmail.com>
2018-03-23 20:05:30 +0800

    :art: yay i done addElement
    a

--- hw2/hw2-3/main.js
+++ hw2/hw2-3/main.js
@@ -1,13 +1,13 @@
 var globalLists = [];
 
 class TodoList {
-  constructor(myDiv, title) {
-    this.myDiv = myDiv;
+  constructor(myid, title) {
+    this.myDiv = document.getElementById(myid);
     this.myTitle = title;
 
-    this.myList = myDiv.getElementsByTagName("LI");
-    this.myHeader = myDiv.getElementsByTagName("h2")[0];
-    this.myAddBtn = myDiv.getElementsByClassName("addBtn")[0];
+    this.myList = this.myDiv.getElementsByTagName("LI");
+    this.myHeader = this.myDiv.getElementsByTagName("h2")[0];
+    this.myAddBtn = this.myDiv.getElementsByClassName("addBtn")[0];
 
     this.initTitle();
     this.initCloseBtn();
@@ -53,40 +53,43 @@ class TodoList {
   }
 
   initAddBtn() {
-    
-    this.myAddBtn.addEventListener('click', function(ev) {
-      console.log("??");
-      // var li = document.createElement("li");
-      // var inputValue = myDiv.getElementsByTagName("INPUT").value;
-      // var t = document.createTextNode(inputValue);
-      // li.appendChild(t);
-      // if (inputValue === '') {
-      //   alert("You must write something!");
-      // } else {
-      //   myDiv.getElementsByTagName("UL")[0].appendChild(li);
-      // }
-      // myDiv.getElementsByTagName("INPUT")[0].value = "";
+    var tmpDiv = this.myDiv;
 
-      // var span = document.createElement("SPAN");
-      // var txt = document.createTextNode("\u00D7");
-      // span.className = "close";
-      // span.appendChild(txt);
-      // li.appendChild(span);
-    
-      // for (i = 0; i < close.length; i++) {
-      //   close[i].onclick = function() {
-      //     var div = this.parentElement;
-      //     div.style.display = "none";
-      //   }
-      // }
+    this.myAddBtn.addEventListener('click', function(ev) {
+      var li = document.createElement("li");
+      var inputValue = tmpDiv.getElementsByTagName("INPUT")[0].value;
+      var t = document.createTextNode(inputValue);
+      li.appendChild(t);
+      
+
+      var span = document.createElement("SPAN");
+      var txt = document.createTextNode("\u00D7");
+
+      span.className = "close";
+      span.appendChild(txt);
+      li.appendChild(span);
+
+      if (inputValue === '') {
+        alert("You must write something!");
+      } else {
+        tmpDiv.getElementsByTagName("UL")[0].appendChild(li);
+      }
+      tmpDiv.getElementsByTagName("INPUT")[0].value = "";
+
+      var close = tmpDiv.getElementsByClassName("close");
+      var i;
+      for (i = 0; i < close.length; i++) {
+        
+        close[i].onclick = function() {
+          var div = this.parentElement;
+          div.style.display = "none";
+        }
+      }
 
     }, false);
   }
 
 }
 
-var myDiv = document.getElementById("list1");
-globalLists.push(new TodoList(myDiv, "adrian1"));
-
-var myDiv = document.getElementById("list2");
-globalLists.push(new TodoList(myDiv, "adrian2"));
+globalLists.push(new TodoList("list1", "adrian1"));
+globalLists.push(new TodoList("list2", "adrian2"));
