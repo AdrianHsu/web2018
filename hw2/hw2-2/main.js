c1e898502113a77d07975b0e0121ab583132c2db
AdrianHsu <adrianhsu1995@gmail.com>
2018-03-27 12:16:32 +0800

    test

--- hw2/hw2-2/main.js
+++ hw2/hw2-2/main.js
@@ -1,24 +1,19 @@
-var day = document.getElementById('day1');
 var date = document.getElementById("day1").getElementsByClassName("date")[0];
+var mybtn = document.getElementById("mybtn");
 
-day.addEventListener('mouseenter', function(){
+mybtn.addEventListener("click", function() {
 	var newbutton = document.createElement("div");
 	var btntext = document.createTextNode("NEW");
 	newbutton.appendChild(btntext);
 	newbutton.className="button_new";
 	var att = document.createAttribute("onclick");
-	att.value = "newunit()"
+	att.value = "creatediv()"
 	newbutton.setAttributeNode(att);
 	date.appendChild(newbutton);
 });
 
-day.addEventListener('mouseleave', function() {
-	date.removeChild(date.lastChild);
-});
 
-function newunit() {
-	console.log("pressed");
-
+function creatediv() {
 	var newdiv = document.createElement("div");
 	newdiv.setAttribute("class", "unit");
 	newdiv.innerHTML = document.getElementById("newunit").innerHTML;
