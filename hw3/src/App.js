1e86e10103a213be80126425924b0912bb916bff
AdrianHsu <adrianhsu1995@gmail.com>
2018-04-05 12:03:26 +0800

    hw3

--- hw3/src/App.js
+++ hw3/src/App.js
@@ -7,7 +7,62 @@ class App extends Component {
     return (
       <div className="App">
         <ReactLogo />
+        <TodoCard />
       </div>
+    );
+  }
+}
+
+class TodoCard extends Component {
+  render() {
+    return (
+      <div className="card" style={{ width: 18 + "rem" }}>
+        <TodoCardHeader />
+        <TodoList />
+      </div>
+    );
+  }
+}
+class TodoCardHeader extends Component {
+  render() {
+    return (
+      <div className="card-header">
+        School Homework
+        <button type="button" className="close" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+    );
+  }
+}
+class TodoList extends Component {
+  render() {
+    return (
+      <div className="list-group list-group-flush">
+        <TodoItem text="MLDS hw1" />
+        <TodoItem text="Web hw3" />
+        <TodoItem text="0bj903jb09jb90" />
+      </div>
+    );
+  }
+}
+
+class TodoItem extends Component {
+  render() {
+    return (
+      <button className="list-group-item list-group-item-action">
+        <s>{this.props.text}</s>
+        <TodoItemDeleteBtn />
+      </button>
+    );
+  }
+}
+class TodoItemDeleteBtn extends Component {
+  render() {
+    return (
+      <button type="button" className="close" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
     );
   }
 }
